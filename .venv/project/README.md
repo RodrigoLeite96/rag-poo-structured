95800817c591217af3ff11ff252fb0f408052891
RodrigoLeite96 <rodrigolt9@gmail.com>
2026-03-05 09:31:37 -0400

    remove venv files

--- .venv/project/README.md
+++
@@ -1,7 +0,0 @@
-# Python Project in .venv
-
-Run:
-
-```bash
-../bin/python3 main.py
-```
--- .venv/project/main.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def main() -> None:
-    print(f"Hello from project inside .venv at {datetime.now().isoformat(timespec='seconds')}")
-
-
-if __name__ == "__main__":
-    main()
